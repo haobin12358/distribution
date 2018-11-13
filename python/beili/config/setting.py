1b74e41c16773a731a78421647edd5c6ffc53d78
fx <13588046059@163.com>
2018-11-14 02:53:13 +0800

    配置

--- python/beili/config/setting.py
+++ python/beili/config/setting.py
@@ -1,33 +1,10 @@
 # -*- coding:utf8 -*-
 SECRET_KEY = '4C2tAoH6fhI17OdNk0u2343fdBIJVBU93ez5DswvGVyprlFWa8PBjqQmRTxJUMKXYb'
 TOKEN_EXPIRATION = 3600 * 2  # token过期时间(秒)
-# # test
-# APP_ID = 'wxe8e8f6b9351d3587'
-# APP_SECRET_KEY = 'b89e22f046d33b39c7a4afa485e661dc'
 
 # 服务器域名
 QRCODEHOSTNAME = 'http://112.74.175.144:7444'
 SERVER = 'https://beiliserver.daaiti.cn'
-
-# # 保证金
-# BAIL = 0
-#
-# # 打款信息
-# ALIPAYNUM = '3480837574@qq.com'
-# ALIPAYNAME = u'章赛娥'
-# WECHAT = 'youknowfx'
-# BANKNAME = u'宁波银行'
-# COUNTNAME = u'冯玉蓓'
-# CARDNUM = '6214 1802 0000 1634 616'
-# AGENTMONEY = 398
-# WECHATSERVICE = 'beilifushi8790'
-# CHARGEBANKNAME = '中国建设银行'
-#
-# # 提现银行信息
-# DRAWBANK = u'中国工商银行'
-#
-# # 直推奖励金额
-# REWARD = 100
 
 # 支付相关
 APP_ID = 'wxe618ef815a774878'
