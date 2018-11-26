bd46e9ba0864613075391bd6b7345176d09904e0
fx <13588046059@163.com>
2018-11-27 02:45:05 +0800

    order

--- python/beili/apis/__init__.py
+++ python/beili/apis/__init__.py
@@ -3,10 +3,10 @@ from control.COrder import COrder
 from control.CAccount import CAccount
 import threading
 
-# print "start timer_fun after 100s!"
-# timer = threading.Timer(100, COrder().timer_fun)  # 首次启动定时器
-# timer.start()
-#
-# print "start deal_profit_fail after 5s!"
-# timer = threading.Timer(5, CAccount().deal_reward_discount)  # 首次启动处理奖金定时器
-# timer.start()
+print "start timer_fun after 100s!"
+timer = threading.Timer(100, COrder().timer_fun)  # 首次启动定时器
+timer.start()
+
+print "start deal_profit_fail after 5s!"
+timer = threading.Timer(5, CAccount().deal_reward_discount)  # 首次启动处理奖金定时器
+timer.start()
