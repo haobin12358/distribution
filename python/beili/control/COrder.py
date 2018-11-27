9e6b5d857211d49e4086269b3fc846c1f587ae27
fx <13588046059@163.com>
2018-11-28 01:36:28 +0800

    fix

--- python/beili/control/COrder.py
+++ python/beili/control/COrder.py
@@ -100,7 +100,7 @@ class COrder():
                     all_psid.append(sku['psid'])
                 product['PRprice'] = check_product['PRprice']
                 new_list.append(product)
-            if totalprice != mount + real_PRlogisticsfee or real_PRlogisticsfee != PRlogisticsfee:
+            if totalprice != round(mount + real_PRlogisticsfee, 2) or real_PRlogisticsfee != PRlogisticsfee:
                 response = {}
                 response['status'] = 200
                 response['success'] = False
