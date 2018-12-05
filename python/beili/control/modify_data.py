4147f0aab7e75bcd6cf86b792847f9b706ee84ed
fx <13588046059@163.com>
2018-12-05 23:53:51 +0800

    fix

--- python/beili/control/modify_data.py
+++ python/beili/control/modify_data.py
@@ -1,22 +1,7 @@
 # *- coding:utf8 *-
 
-import re
 import sys
 import os
-import uuid
-import random
-import shutil
-import xlrd
-import xlwt
-from flask import request
-# import logging
-import threading
-from config.response import PARAMS_MISS, SYSTEM_ERROR, PARAMS_ERROR, TOKEN_ERROR, AUTHORITY_ERROR, STOCK_NOT_ENOUGH,\
-        NO_ENOUGH_MOUNT, NO_BAIL, NO_ADDRESS, NOT_FOUND_ORDER, PRODUCT_OFFLINE, SKU_WRONG, CANNOT_CANCEL
-from config.setting import QRCODEHOSTNAME
-from common.token_required import verify_token_decorator, usid_to_token, is_tourist, is_admin
-from common.import_status import import_status
-from common.timeformat import get_db_time_str
 from common.get_model_return_list import get_model_return_list, get_model_return_dict
 from service.SUser import SUser
 from service.SMessage import SMessage
@@ -25,15 +10,8 @@ from service.SGoods import SGoods
 from service.SMyCenter import SMyCenter
 from service.DBSession import db_session
 from service.SAccount import SAccount
-import platform
-from configparser import ConfigParser
-from common.beili_error import stockerror, dberror
-from datetime import datetime
-from common.timeformat import format_for_db, get_random_str, get_random_int
 from models.model import User, AgentMessage, Performance, Amount, Reward, MoneyRecord, OrderSkuInfo, ShoppingCart\
     , ProductSku, OrderInfo
-
-sys.path.append(os.path.dirname(os.getcwd()))
 
 class modify_data():
     def __init__(self):
@@ -82,5 +60,4 @@ class modify_data():
         finally:
             session.close()
 
-if __name__ == "__main__":
-    modify_data().modify()
+modify_data().modify()
