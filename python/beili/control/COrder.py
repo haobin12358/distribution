ad1dab485cb2b671b0beb1604b71663f1a1e78e9
fx <13588046059@163.com>
2018-12-18 03:32:50 +0800

    fix

--- python/beili/control/COrder.py
+++ python/beili/control/COrder.py
@@ -546,11 +546,11 @@ class COrder():
         name = ''
         for product in list:
             sku_list = get_model_return_list(self.sorder.get_sku_list_by_opiid(product['OPIid']))
+            productName = product['PRname'].split("返")[0].strip()
+            result = re.match('[0-9A-Za-z]+', productName)
+            if result:
+                productName = result.group()
             for sku in sku_list:
-                productName = product['PRname'].split("返")[0].strip()
-                result = re.match('[0-9A-Za-z]+', productName)
-                if result:
-                    productName = result.group()
                 name = name + productName + sku['colorname'] + sku['sizename'] + str(sku['number']) + u'件' + ' '
         return name
 
